793c65ee960bbab61c3a249d1e460990312998a4
sajedeh_taheri <82841130+sajedeh-dev@users.noreply.github.com>
2025-09-21 20:54:10 +0330

    new style

--- components/template/SigninPage.js
+++ components/template/SigninPage.js
@@ -37,7 +37,7 @@ function SigninPage() {
       </h3>
       <input
         className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg 
-             focus:ring-2 focus:ring-blue-500 focus:border-blue-500 
+              focus:outline-none focus:border-pink-300 focus:ring-2 focus:ring-pink-300 
              block w-64 p-2.5"
         type="text"
         placeholder="Email"
@@ -45,7 +45,7 @@ function SigninPage() {
         onChange={(e) => setEmail(e.target.value)}
       />
       <input
-        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-64 p-2.5 "
+        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:outline-none focus:border-pink-300 focus:ring-2 focus:ring-pink-300 block w-64 p-2.5 "
         type="password"
         placeholder="Password"
         value={password}
