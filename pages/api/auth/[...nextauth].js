5e758343f5752b8295fb370cb5a8692067952c25
sajedeh_taheri <82841130+sajedeh-dev@users.noreply.github.com>
2025-10-02 16:07:45 +0330

    edit code

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,43 +1,3 @@
-// import NextAuth from "next-auth";
-// import CredentialsProvider from "next-auth/providers/credentials";
-
-// import User from "../../../model/User";
-// import { verifyPassword } from "../../../utils/auth";
-// import conectDB from "../../../utils/conectDB";
-
-// const authOptions = {
-//   session: { strategy: "jwt" },
-//   providers: [
-//     CredentialsProvider({
-//       async authorize(credentials, req) {
-//         const { email, password } = credentials;
-
-//         try {
-//           await conectDB();
-//         } catch (error) {
-//           throw new Error("Error in connecting to DB!");
-//         }
-
-//         if (!email || !password) {
-//           throw new Error("Invalid Data!");
-//         }
-
-//         const user = await User.findOne({ email: email });
-
-//         if (!user) throw new Error("User doesn't exist!");
-
-//         const isValid = await verifyPassword(password, user.password);
-
-//         if (!isValid) throw new Error("Username or password is incorrect!");
-
-//         return { email };
-//       },
-//     }),
-//   ],
-// };
-
-// export default NextAuth(authOptions);
-
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 
@@ -78,3 +38,44 @@ const authOptions = {
 };
 
 export default NextAuth(authOptions);
+
+// import NextAuth from "next-auth";
+// import CredentialsProvider from "next-auth/providers/credentials";
+
+// import User from "../../../model/User";
+// import { verifyPassword } from "../../../utils/auth";
+// import conectDB from "../../../utils/conectDB";
+
+// const authOptions = {
+//   session: { strategy: "jwt" },
+//   providers: [
+//     CredentialsProvider({
+//       async authorize(credentials, req) {
+//         const { email, password } = credentials;
+
+//         try {
+//           await conectDB();
+//         } catch (error) {
+//           throw new Error("Error in connecting to DB!");
+//         }
+
+//         if (!email || !password) {
+//           throw new Error("Invalid Data!");
+//         }
+
+//         const user = await User.findOne({ email: email });
+
+//         if (!user) throw new Error("User doesn't exist!");
+
+//         const isValid = await verifyPassword(password, user.password);
+
+//         if (!isValid) throw new Error("Username or password is incorrect!");
+
+//         return { email };
+//       },
+//     }),
+//   ],
+//   secret: process.env.NEXTAUTH_SECRET, // ← اضافه شد
+// };
+
+// export default NextAuth(authOptions);
